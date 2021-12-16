4332f3e2bbed1abf116d753e2bb1a99e245ccada
jasonasante <jason.asante@outlook.com>
2021-12-16 20:13:01 +0000

    Ascii-art-justify

---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ToLower(s string) string {
+	lowstr := []rune(s)
+	for i, char := range lowstr {
+		if char >= 65 && char <= 90 {
+			lowstr[i] = lowstr[i] + 32
+		}
+	}
+	return string(lowstr)
+}
+
+func main() {
+	var emptyString string
+	var inputString []string
+	if len(os.Args) == 4 {
+		inputString = strings.Split(os.Args[1], "\\n")
+		// this takes the argument that we are printing and seperates them into a []string via \n
+		// this will then therefore automatically will print each []string on a new line.
+	} else {
+		fmt.Println("Usage: go run . [STRING] [BANNER] [OPTION]")
+		fmt.Println("EX: go run . something standard --align=right")
+		os.Exit(0)
+	}
+	Content, _ := os.ReadFile(os.Args[2] + ".txt")
+	asciiSlice2 := make([][]string, 95)
+	// this stores the ascii-bubbles in order of the
+	// there are 95 ascii characters and this lets us index the dimension holding each bubble
+	for i := 0; i < len(asciiSlice2); i++ {
+		asciiSlice2[i] = make([]string, 9)
+	}
+	// this makes the asciiSlice2[i] have a length of 8
+	var bubbleCount int
+	count := 0
+	for i := 1; i < len(Content); i++ {
+		if Content[i] == '\n' && bubbleCount <= 94 {
+			asciiSlice2[bubbleCount][count] = emptyString
+			// so bubbleCount is the index and count is the row
+			// so asciiSlice2[1][0] is the 1st row of the exclamation mark
+			emptyString = ""
+			count++
+			// we want count to get to 8 as that is the number of rows (height of the 8)
+		}
+		if count == 9 {
+			count = 0
+			bubbleCount++
+			// i++
+			// once we have the 8 rows of the bubble text, we want to move onto the next index of the
+			// asciiSlice2, hence bubbleCount++
+			// We also have i++
+		} else {
+			if Content[i] != '\n' && Content[i] != '\r' {
+				emptyString += string(Content[i])
+				// as count != 8 and Contet[i]!= '\n', it will append the contents of each row.
+				// Once it reaches the '\n' at the end of the row, the first if statement is activated.
+			}
+		}
+	}
+	terminalSize := int(os.Stdout.Fd())
+	fmt.Println(terminalSize)
+	var alignFlag []string
+	estrCount := 0
+	var tempOutput [][]string
+	// // why is it that when we used make, it did not print the first index?
+	if strings.HasPrefix(os.Args[3], "--align=") {
+		alignFlag = strings.Split(os.Args[3], "--align=")
+	} else {
+		fmt.Println("Usage: go run . [STRING] [BANNER] [OPTION]")
+		fmt.Println("EX: go run . something standard --align=right")
+		os.Exit(0)
+	}
+
+	alignFlag[1] = ToLower(alignFlag[1])
+	fmt.Println(alignFlag[1])
+	if (alignFlag[1] != "center") {
+		fmt.Println("Usage: go run . [STRING] [BANNER] [OPTION]")
+		fmt.Println("EX: go run . something standard --align=right")
+		os.Exit(0)
+	}
+	if (alignFlag[1] != "right") {
+		fmt.Println("Usage: go run . [STRING] [BANNER] [OPTION]")
+		fmt.Println("EX: go run . something standard --align=right")
+		os.Exit(0)
+	}
+	if (alignFlag[1] != "left") {
+		fmt.Println("Usage: go run . [STRING] [BANNER] [OPTION]")
+		fmt.Println("EX: go run . something standard --align=right")
+		os.Exit(0)
+	}
+	if (alignFlag[1] != "justify") {
+		fmt.Println("Usage: go run . [STRING] [BANNER] [OPTION]")
+		fmt.Println("EX: go run . something standard --align=right")
+		os.Exit(0)
+	}
+
+	for j, str := range inputString {
+		for _, aRune := range str {
+			tempOutput = append(tempOutput, asciiSlice2[aRune-rune(32)])
+			// due to the loop it will append the bubble eqivalent of the every letter inside inputString
+		}
+		for i := range tempOutput[0] {
+			for _, char := range tempOutput {
+				if alignFlag[1] == "center" && estrCount == 0 {
+					fmt.Printf("%100v", char[i])
+				} else {
+					fmt.Print(char[i])
+				}
+				//now figure how how to replace %110v with the
+				estrCount++
+				if estrCount == len(inputString[j]) {
+					estrCount = 0
+				}
+				//fmt.Print(char[i])
+				// // this prints each line of each bubble letter (which is each slice of string)
+			}
+			fmt.Println()
+		}
+		tempOutput = nil
+		// once the word has been printed, we want to reset tempOutput to nil, ready to be filled
+		// by the next string element in inputString.
+	}
+}
